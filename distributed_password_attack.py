706ea4c478026cbde6cfcd5e9834b1a0eaf76b90
yazeed <yazoid1421@gmail.com>
2024-04-24 20:02:16 +0300

    Password Found !!

--- distributed_password_attack.py
+++ distributed_password_attack.py
@@ -9,7 +9,7 @@ def main():
 
     # Define CSV file and chunk size
     filename = '10millionPasswords.csv'
-    chunk_size = 1000  # Adjust as needed
+    chunk_size = 1000
     target_password = '1212'  # Define the target password
 
     found_password = False
@@ -19,32 +19,40 @@ def main():
         chunks = pd.read_csv(filename, chunksize=chunk_size)
 
         current_process = 1
-        # Distribute chunks to other processes
         for chunk_data in chunks:
-            if comm.bcast(found_password, root=0):
-                print(f"Password found in process {target_password}!")
-                break  # Stop if password is found
-            comm.send(chunk_data, dest=current_process)
-            current_process = (current_process % (size - 1)) + 1
-
-        print('All chunks have been sent to all processes')
-        # Signal end of chunks to all processes
-        for i in range(1, size):
-            comm.send(None, dest=i)
-
-    else:
-        # Receive chunks in worker processes
-        while True:
-            chunk_data = comm.recv(source=0)
-            print(f"Process {rank} received chunk data")
-            if chunk_data is None:
+            # Before sending new data, check if password has been found using a non-blocking receive
+            status = MPI.Status()
+            flag = comm.Iprobe(source=MPI.ANY_SOURCE, tag=0, status=status)
+            if flag:
+                comm.recv(source=status.Get_source(), tag=0)
+                print(f"Password found! Stopping all processes.")
                 break
 
-            # Process chunk data
-            if target_password in chunk_data['password'].values:
-                found_password = True
-                print(f"Password found in process {rank}!")
-                comm.bcast(found_password, root=rank)  # Send stop signal
+            comm.send(chunk_data, dest=current_process, tag=1)
+            current_process = (current_process % (size - 1)) + 1
+
+        # Signal end of chunks to all processes
+        for i in range(1, size):
+            comm.send(None, dest=i, tag=1)
+
+    else:
+        while True:
+            status = MPI.Status()
+            chunk_data = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            if status.Get_tag() == 1:
+                if chunk_data is None:
+                    break
+
+                # Process chunk data
+                if target_password in chunk_data['password'].values:
+                    found_password = True
+                    print(f"Password found in process {rank}!")
+                    for i in range(size):
+                        comm.send(True, dest=i, tag=0)
+                    break
+
+            elif status.Get_tag() == 0:
+                break  # Receive termination signal
 
 if __name__ == "__main__":
     main()
